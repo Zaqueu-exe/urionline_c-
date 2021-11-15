911af855af4828945b313b61180cc824e09daeff
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2021-11-15 19:12:40 -0300

    Add files via upload

---
+++ iniciante/URI_1000.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri.iniciante
+{
+    internal class URI_1000
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
