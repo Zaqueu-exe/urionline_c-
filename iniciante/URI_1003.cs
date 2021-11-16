eebc31b6f44839c4e7b70961393b3ab80a3cc435
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2021-11-15 21:45:22 -0300

    Add files via upload

---
+++ iniciante/URI_1003.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri.iniciante
+{
+    internal class URI_1003
+    {
+        static void Main(String[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("SOMA = " + (n1 + n2));
+
+        }
+    }
+}
