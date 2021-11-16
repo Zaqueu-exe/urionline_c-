07b0172770d12e133701285afc7eda1945f813b8
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2021-11-16 17:18:04 -0300

    Add files via upload

---
+++ iniciante/URI_1008.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri.iniciante
+{
+    internal class URI_1008
+    {
+        static void Main(String[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            float n3 = float.Parse(Console.ReadLine());
+            Console.WriteLine("NUMBER = " + n1);
+            Console.WriteLine("SALARY = U$ " + (n2 * n3).ToString("F2"));
+
+
+        }
+    }
+}
