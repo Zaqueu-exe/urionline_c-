a60bdb13df3252e4936f651634a8353e9f4b1519
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2021-11-16 17:04:26 -0300

    Add files via upload

---
+++ iniciante/URI_1006.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri.iniciante
+{
+    internal class URI_1006
+    {
+        static void Main(String[] args)
+        {
+            float n1 = float.Parse(Console.ReadLine());
+            float n2 = float.Parse(Console.ReadLine());
+            float n3 = float.Parse(Console.ReadLine());
+            Console.WriteLine("MEDIA = " + (((n1 * 2.0) + (n2 * 3.0) + (n3 * 5.0)) / 10).ToString("F1"));
+
+
+        }
+    }
+}
