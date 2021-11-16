4ef031470a21546cf74ce8d848c5350e6ce29fbe
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2021-11-15 21:42:22 -0300

    Add files via upload

---
+++ iniciante/URI_1002.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri.iniciante
+{
+    internal class URI_1002
+    {
+        static void Main(String[] args)
+        {
+            double area = double.Parse(Console.ReadLine());
+            Console.WriteLine("A=" + ((area * area) * 3.14159).ToString("F4"));
+
+        }
+    }
+}
