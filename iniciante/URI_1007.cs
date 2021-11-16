135441ab973dde88b4ddc81a89b556f7ff508b36
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2021-11-16 17:08:50 -0300

    Add files via upload

---
+++ iniciante/URI_1007.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri.iniciante
+{
+    internal class URI_1007
+    {
+        static void Main(String[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("DIFERENCA = " + (n1 * n2 - n3 * n4));
+
+
+        }
+    }
+}
