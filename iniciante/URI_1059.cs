f677417da8c0e0aeb539b139a9da98942768df2f
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2021-12-03 12:50:15 -0300

    Create URI_1059.cs

---
+++ iniciante/URI_1059.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri.iniciante
+{
+    internal class URI_1046
+    {
+        static void Main(String[] args)
+        {
+            for( int i= 1; i <=100; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
