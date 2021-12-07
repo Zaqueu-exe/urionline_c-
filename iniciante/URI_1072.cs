5dc633c9d6c1c00407f294ba249d28652e689c41
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2021-12-07 15:27:14 -0300

    Create URI_1072.cs

---
+++ iniciante/URI_1072.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri.iniciante
+{
+    internal class URI_1046
+    {
+        static void Main(String[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int dentro = 0, fora = 0;
+            for(int i = 0; i < n; i++)
+            {
+                int aux = int.Parse(Console.ReadLine());
+                if(aux >= 10 && aux <= 20)
+                {
+                    dentro++;
+                }
+                else
+                {
+                    fora++;
+                }
+            }
+            Console.WriteLine(dentro + " in");
+            Console.WriteLine(fora + " out");
+        }
+    }
+}
