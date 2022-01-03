90ba05e1d3a27b940aa99b50b7b6c2fc375c34a4
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2022-01-03 10:52:54 -0300

    Create URI_1097.cs

---
+++ iniciante/URI_1097.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri.iniciante
+{
+    internal class URI_1046
+    {
+        static void Main(String[] args)
+        {
+            int i = 1, j = 7;
+            while(i <= 9)
+            {
+                for(int g = 0; g < 3; g++)
+                {
+                    Console.WriteLine($"I={i} J={j--}");
+
+                }
+                j += 5;
+                i += 2;
+            }
+
+        }
+    }
+}
+
