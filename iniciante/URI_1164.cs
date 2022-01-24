1410fcbb1b48e0ba50cfb128a8c1c2eb9e0d908a
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2022-01-24 10:33:23 -0300

    Create URI_1164.cs

---
+++ iniciante/URI_1164.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri.iniciante
+{
+    internal class URI_1046
+    {
+        public static void Main(String[] args)
+        {
+            int aux = int.Parse(Console.ReadLine());
+            for (int p = 0; p < aux; p++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int sum = 0;
+                for (int i = 1; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                if (sum == n)
+                {
+                    Console.WriteLine(n + " eh perfeito");
+                }
+                else
+                {
+                    Console.WriteLine(n + " nao eh perfeito");
+                }
+            }
+        }
+    }
+}
+
