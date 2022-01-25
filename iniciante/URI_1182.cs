3aaf1955498cd45525668c3a419ea05f9c0ee6bb
Zaqueu Alves <61361618+Zaqueu-exe@users.noreply.github.com>
2022-01-25 10:24:44 -0300

    Create URI_1182.cs

---
+++ iniciante/URI_1182.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace uri.iniciante
+{
+    internal class URI_1046
+    {
+        public static void Main(String[] args)
+        {
+            
+            int linha = int.Parse(Console.ReadLine());
+            string op = Console.ReadLine();
+            double[,] m = new double[12, 12];
+            double s = 0.0;
+            for(int i = 0; i < m.GetLength(0); i++)
+            {
+                for(int t = 0; t < m.GetLength(1); t++)
+                {
+                    m[i, t] = double.Parse(Console.ReadLine());
+                    if(linha == t)
+                    {
+                        s += m[i, t];
+                    }
+                }
+            }
+            if (op.Equals("S"))
+            {
+                Console.WriteLine((s).ToString("0.0"));
+            }
+            else
+            {
+                Console.WriteLine((s/12.0).ToString("0.0"));
+            }
+
+        }
+    }
+}
+
